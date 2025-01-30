8b7fd4c3a9f2ad42c1ad702416326a46fca959d6
Eli Weissler <eliweissler@gmail.com>
2025-01-29 21:11:18 -0500

    added julia pronto

---
+++ quantum-optimal-control/pronto/julia_x_gate.jl
@@ -0,0 +1,70 @@
+using Pkg
+Pkg.activate(".")
+
+# Will have to run once
+# ENV["PYTHON"] = "./anaconda3/bin/python"
+# Pkg.build("PyCall")
+
+using QuantumOptics, NPZ, PyPlot
+include("gate_utils.jl")
+# Truncate Hamiltonian Function
+function truncate(H0, hspace)
+    H0[hspace .+ 1, hspace .+ 1]
+end
+
+
+
+drive = "theta"
+params = npzread("H_$drive.npz")
+
+# The .+ 1 is elementwise addition to an array/vector
+# Must do .+ 1 because Julia indexes from 1
+hspace_full = params["hspace_full"] 
+hspace_reduced = params["hspace_reduced"] 
+w_trans_1 = params["w_trans_1"]
+w_trans_2 = params["w_trans_2"]
+drive_term = params["drive"]
+H0 = params["H0"]
+
+basis = NLevelBasis(length(hspace_reduced))
+H_trunc = Operator(basis, truncate(H0, hspace_reduced))
+drive_trunc = Operator(basis, truncate(drive_term, hspace_reduced))
+
+
+
+H = LazySum([1.0, 0.0, 0.0], [H_trunc, drive_trunc, drive_trunc])
+function Ht(t, psi)
+    H.factors[2] = drive_gauss(t, params["drive_freq_A"], params["gate_time"], params["drive_amp_A"])
+    H.factors[3] = drive_gauss(t, params["drive_freq_B"], params["gate_time"], params["drive_amp_B"])
+    return H
+end
+
+
+tlist = LinRange(0,params["gate_time"], 1000)
+
+props, times = batch_evol(Ht, [0, 2], tlist)
+
+pops, f = plot_evolution(hspace_reduced, tlist, props, [0, 2], 7, savename="theta_gate.png");
+
+plt.gca().set_yscale("log")
+plt.gca().set_ylim(1e-05, 1)
+plt.savefig("theta_gate.png")
+
+# #Expectation values
+# function calc_pops(t, psi)
+#     p1 = abs(psi.data[1])
+#     p2 =  abs(psi.data[2])
+#     p3 =  abs(psi.data[3])
+#     return p1, p2, p3
+# end
+
+# psi0 = nlevelstate(basis, 1)
+# tout, pops = timeevolution.schroedinger_dynamic(tlist, psi0, Ht; fout=calc_pops)
+
+# f, ax = plt.subplots(ncols=2)
+# ax[1].plot(tlist, drive_gauss.(tlist, params["drive_freq_A"], params["gate_time"], params["drive_amp_A"]))
+# for i in range(1,3)
+#     ax[2].plot(tlist, [x[i] for x in pops])
+# end
+# plt.draw()
+# plt.savefig("theta_gate_2.png")
---
+++ quantum-optimal-control/pronto/pronto_example.jl
@@ -0,0 +1,102 @@
+using Pkg
+Pkg.activate(".")
+
+using PyPlot
+
+using PRONTO
+using LinearAlgebra
+using StaticArrays
+using Base: @kwdef
+
+## ----------------------------------- define helper functions ----------------------------------- ##
+
+function mprod(x)
+    Re = I(2)
+    Im = [0 -1;
+          1 0]
+    M = kron(Re,real(x)) + kron(Im,imag(x))
+    return M
+end
+
+## ----------------------------------- define the model ----------------------------------- ##
+
+@kwdef struct XGate3 <: PRONTO.Model{12,1}
+    kl::Float64 = 0.01
+    kq::Float64 = 0.5
+end
+
+@define_f XGate3 begin
+    E0 = 0.0
+    E1 = 1.0
+    E2 = 5.0
+    H0 = diagm([E0, E1, E2])
+    H00 = kron(I(2),H0)
+    a1 = 0.1
+    a2 = 0.5
+    a3 = 0.3
+    Ω1 = a1 * u[1]
+    Ω2 = a2 * u[1]
+    Ω3 = a3 * u[1]
+    H1 = [0 Ω1 Ω3; Ω1 0 Ω2; Ω3 Ω2 0]
+    H11 = kron(I(2),H1)
+    return 2 * π * mprod(-im * (H00 + H11)) * x
+end
+
+@define_l XGate3 begin
+    kl/2*u'*I*u + kq/2*x'*mprod(diagm([0,0,1,0,0,1]))*x
+end
+
+@define_m XGate3 begin
+    ψ1 = [1;0;0]
+    ψ2 = [0;1;0]
+    xf = vec([ψ2;ψ1;0*ψ2;0*ψ1])
+    return 1/2*(x-xf)'*I(12)*(x-xf)
+end
+
+@define_Q XGate3 I(12)
+@define_R XGate3 I(1)
+PRONTO.Pf(θ::XGate3,α,μ,tf) = SMatrix{12,12,Float64}(I(12))
+
+resolve_model(XGate3)
+
+## ----------------------------------- run optimization ----------------------------------- ##
+
+θ = XGate3()
+τ = t0,tf = 0,10
+ψ1 = [1;0;0]
+ψ2 = [0;1;0]
+x0 = SVector{12}(vec([ψ1;ψ2;0*ψ1;0*ψ2]))
+μ = t->SVector{1}((π/tf)*exp(-(t-tf/2)^2/(tf^2))*cos(2*π*1*t))
+η = open_loop(θ, x0, μ, τ) # guess trajectory
+ξ,data = pronto(θ, x0, η, τ;tol=1e-4); # optimal trajectory
+
+## ----------------------------------- plot results ----------------------------------- ##
+
+fig, ax = plt.subplots(nrows=3)
+ts = range(t0,tf,length=1001)
+
+(ax1, ax2, ax3) = ax
+
+for a in ax
+    a.set_xlabel("time")
+end
+
+ax1.set_ylabel("control input")
+ax2.set_ylabel("population")
+ax3.set_ylabel("population")
+
+ax1.plot(ts, [ξ.u(t)[1] for t in ts], linewidth = 2)
+
+ax2.plot(ts, [ξ.x(t)[1]^2+ξ.x(t)[7]^2 for t in ts], linewidth = 2, label = "|0⟩")
+ax2.plot(ts, [ξ.x(t)[2]^2+ξ.x(t)[8]^2 for t in ts], linewidth = 2, label = "|1⟩")
+ax2.plot(ts, [ξ.x(t)[3]^2+ξ.x(t)[9]^2 for t in ts], linewidth = 2, label = "|2⟩")
+ax2.legend()
+
+ax3.plot(ts, [ξ.x(t)[4]^2+ξ.x(t)[10]^2 for t in ts], linewidth = 2, label = "|0⟩")
+ax3.plot(ts, [ξ.x(t)[5]^2+ξ.x(t)[11]^2 for t in ts], linewidth = 2, label = "|1⟩")
+ax3.plot(ts, [ξ.x(t)[6]^2+ξ.x(t)[12]^2 for t in ts], linewidth = 2, label = "|2⟩")
+ax3.legend()
+
+plt.tight_layout()
+
+plt.savefig("control_plot.png")
